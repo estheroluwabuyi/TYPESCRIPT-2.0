d512e06bc16387a6cc93e3753a9909421ab12f96
Esther Okiki <estherokiki15@gmail.com>
2025-03-05 23:40:36 +0100

    functions, param values, default param values

--- build/js/main.js
+++ build/js/main.js
@@ -1,91 +1,47 @@
 "use strict";
-// // let username = "Esther";
-// // console.log(username);
-// // let a: number = 12;
-// // let b: number = 6;
-// // let c: number = 2;
-// // console.log(a / b);
-// // console.log(c * b);
-// let myName: string = "Esther";
-// let meaningOfLife: number;
-// let isLoading: boolean;
-// // let album: string | number;
-// let album: any;
-// myName = "John";
-// meaningOfLife = 42;
-// isLoading = true;
-// album = 1094;
-// const sum = (a: number, b: string) => {
-//   return a + b;
+// // // let username = "Esther";
+// // // console.log(username);
+// interface PostId = stringOrNumber //wont work!
+//Literal Types
+let myName;
+myName = "Esther";
+let userName;
+userName = "Dave";
+// FUNCTIONS
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("Hello!");
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+console.log(multiply(3, 5));
+//optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined")
+        return a + b + c;
+    return a + b;
+};
+//default param values
+// const sumAll = (a: number, b: number, c: number = 2): number => {
+//   return a + b + c;
 // };
-// let postId: string | number;
-// let isActive: number | boolean | string;
-// let re: RegExp = /\w+/g;
-// console.log(re);
-//Arrays
-let stringArr = ["one", "hey", "David"];
-let guitars = ["Strat", "Les Paul", 5150];
-let mixedData = ["EVH", 1984, true];
-stringArr[0] = "John";
-stringArr.push("hey");
-console.log(stringArr);
-guitars[0] = 1984;
-guitars.unshift("Jim");
-console.log(guitars);
-guitars = stringArr;
-mixedData = guitars;
-let test = [];
-let bands = [];
-bands.push("Tom Cruise");
-//Tuple
-let myTuple = ["Dave", 42, true];
-let mixed = ["John", 1, false];
-mixed = myTuple;
-// myTuple = mixed;
-// myTuple[0] = "Danny"; //error
-// myTuple[2] = "Gigi"; //error
-console.log(myTuple);
-//Objects
-let myObj;
-// myObj = "";
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-myObj = {};
-const exampleObj = {
-    prop1: "Dave",
-    prop2: true,
+// logMsg(addAll(2, 3, 2));
+// logMsg(addAll(2, 3));
+// logMsg(sumAll(2, 3));
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
 };
-// exampleObj.prop2 = 43;
-exampleObj.prop2 = false;
-// exampleObj.prop1 = 55
-exampleObj.prop1 = "John";
-let evh = {
-    name: "Eddie",
-    active: false,
-    albums: [1984, 5150, "OU756"],
-};
-let JP = {
-    //   name: "Jimmy",
-    active: true,
-    albums: ["1", "II", "OU756"],
-};
-// evh = JP;
-const greetGuitarist = (guitarist) => {
-    //   return `Hello ${guitarist.name?.toUpperCase()}!`; //optional chaining
-    if (guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}!`;
-    }
-    return "Hello!";
-};
-console.log(greetGuitarist(JP));
-//Enums
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 10] = "U";
-    Grade[Grade["D"] = 11] = "D";
-    Grade[Grade["C"] = 12] = "C";
-    Grade[Grade["B"] = 13] = "B";
-    Grade[Grade["A"] = 14] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
+logMsg(sumAll(undefined, 3));
+//Rest Parameters
--- src/main.ts
+++ src/main.ts
@@ -1,133 +1,207 @@
-// // let username = "Esther";
-// // console.log(username);
+// // // let username = "Esther";
+// // // console.log(username);
 
-// // let a: number = 12;
-// // let b: number = 6;
-// // let c: number = 2;
+// // // let a: number = 12;
+// // // let b: number = 6;
+// // // let c: number = 2;
 
-// // console.log(a / b);
+// // // console.log(a / b);
 
-// // console.log(c * b);
+// // // console.log(c * b);
 
-// let myName: string = "Esther";
-// let meaningOfLife: number;
-// let isLoading: boolean;
-// // let album: string | number;
-// let album: any;
+// // let myName: string = "Esther";
+// // let meaningOfLife: number;
+// // let isLoading: boolean;
+// // // let album: string | number;
+// // let album: any;
 
-// myName = "John";
-// meaningOfLife = 42;
-// isLoading = true;
-// album = 1094;
+// // myName = "John";
+// // meaningOfLife = 42;
+// // isLoading = true;
+// // album = 1094;
 
-// const sum = (a: number, b: string) => {
-//   return a + b;
+// // const sum = (a: number, b: string) => {
+// //   return a + b;
+// // };
+
+// // let postId: string | number;
+// // let isActive: number | boolean | string;
+
+// // let re: RegExp = /\w+/g;
+// // console.log(re);
+
+// //Arrays
+// let stringArr = ["one", "hey", "David"];
+
+// let guitars = ["Strat", "Les Paul", 5150];
+
+// let mixedData = ["EVH", 1984, true];
+
+// stringArr[0] = "John";
+// stringArr.push("hey");
+// console.log(stringArr);
+
+// guitars[0] = 1984;
+// guitars.unshift("Jim");
+// console.log(guitars);
+
+// guitars = stringArr;
+// mixedData = guitars;
+
+// let test = [];
+// let bands: string[] = [];
+// bands.push("Tom Cruise");
+
+// //Tuple
+// let myTuple: [string, number, boolean] = ["Dave", 42, true];
+
+// let mixed = ["John", 1, false];
+
+// mixed = myTuple;
+// // myTuple = mixed;
+
+// // myTuple[0] = "Danny"; //error
+// // myTuple[2] = "Gigi"; //error
+
+// console.log(myTuple);
+
+// //Objects
+// let myObj: object;
+// // myObj = "";
+// myObj = [];
+// console.log(typeof myObj);
+// myObj = bands;
+// myObj = {};
+
+// const exampleObj = {
+//   prop1: "Dave",
+//   prop2: true,
 // };
 
-// let postId: string | number;
-// let isActive: number | boolean | string;
+// // exampleObj.prop2 = 43;
+// exampleObj.prop2 = false;
+// // exampleObj.prop1 = 55
+// exampleObj.prop1 = "John";
 
-// let re: RegExp = /\w+/g;
-// console.log(re);
+// // type Guitarist = {
+// //   name: string;
+// //   //active?: boolean;
+// //   active?: boolean;
+// //   albums: (string | number)[];
+// // };
 
-//Arrays
-let stringArr = ["one", "hey", "David"];
-
-let guitars = ["Strat", "Les Paul", 5150];
-
-let mixedData = ["EVH", 1984, true];
-
-stringArr[0] = "John";
-stringArr.push("hey");
-console.log(stringArr);
-
-guitars[0] = 1984;
-guitars.unshift("Jim");
-console.log(guitars);
-
-guitars = stringArr;
-mixedData = guitars;
-
-let test = [];
-let bands: string[] = [];
-bands.push("Tom Cruise");
-
-//Tuple
-let myTuple: [string, number, boolean] = ["Dave", 42, true];
-
-let mixed = ["John", 1, false];
-
-mixed = myTuple;
-// myTuple = mixed;
-
-// myTuple[0] = "Danny"; //error
-// myTuple[2] = "Gigi"; //error
-
-console.log(myTuple);
-
-//Objects
-let myObj: object;
-// myObj = "";
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-myObj = {};
-
-const exampleObj = {
-  prop1: "Dave",
-  prop2: true,
-};
-
-// exampleObj.prop2 = 43;
-exampleObj.prop2 = false;
-// exampleObj.prop1 = 55
-exampleObj.prop1 = "John";
-
-// type Guitarist = {
-//   name: string;
-//   //active?: boolean;
-//   active?: boolean;
+// interface Guitarist {
+//   name?: string;
+//   active: boolean;
+//   //   active?: boolean;
 //   albums: (string | number)[];
+// }
+
+// let evh: Guitarist = {
+//   name: "Eddie",
+//   active: false,
+//   albums: [1984, 5150, "OU756"],
 // };
 
-interface Guitarist {
+// let JP: Guitarist = {
+//   //   name: "Jimmy",
+//   active: true,
+//   albums: ["1", "II", "OU756"],
+// };
+
+// // evh = JP;
+
+// const greetGuitarist = (guitarist: Guitarist) => {
+//   //   return `Hello ${guitarist.name?.toUpperCase()}!`; //optional chaining
+//   if (guitarist.name) {
+//     return `Hello ${guitarist.name.toUpperCase()}!`;
+//   }
+//   return "Hello!";
+// };
+
+// console.log(greetGuitarist(JP));
+
+// //Enums
+// enum Grade {
+//   U = 10,
+//   D,
+//   C,
+//   B,
+//   A,
+// }
+
+// console.log(Grade.U);
+
+//Type Aliases
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = string | number;
+
+type Guitarist = {
   name?: string;
   active: boolean;
-  //   active?: boolean;
-  albums: (string | number)[];
-}
-
-let evh: Guitarist = {
-  name: "Eddie",
-  active: false,
-  albums: [1984, 5150, "OU756"],
+  albums: stringOrNumber;
 };
 
-let JP: Guitarist = {
-  //   name: "Jimmy",
-  active: true,
-  albums: ["1", "II", "OU756"],
+type UserId = stringOrNumber;
+
+// interface PostId = stringOrNumber //wont work!
+
+//Literal Types
+let myName: "Esther";
+myName = "Esther";
+let userName: "Dave" | "Esther" | "Jade";
+
+userName = "Dave";
+
+// FUNCTIONS
+const add = (a: number, b: number): number => {
+  return a + b;
 };
 
-// evh = JP;
-
-const greetGuitarist = (guitarist: Guitarist) => {
-  //   return `Hello ${guitarist.name?.toUpperCase()}!`; //optional chaining
-  if (guitarist.name) {
-    return `Hello ${guitarist.name.toUpperCase()}!`;
-  }
-  return "Hello!";
+const logMsg = (message: any): void => {
+  console.log(message);
 };
 
-console.log(greetGuitarist(JP));
+logMsg("Hello!");
+logMsg(add(2, 3));
 
-//Enums
-enum Grade {
-  U = 10,
-  D,
-  C,
-  B,
-  A,
-}
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
 
-console.log(Grade.U);
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+console.log(multiply(3, 5));
+
+//optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") return a + b + c;
+
+  return a + b;
+};
+
+//default param values
+// const sumAll = (a: number, b: number, c: number = 2): number => {
+//   return a + b + c;
+// };
+
+// logMsg(addAll(2, 3, 2));
+// logMsg(addAll(2, 3));
+// logMsg(sumAll(2, 3));
+
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(sumAll(undefined, 3));
+
+//Rest Parameters
