c9e010da662d809c6d67eff3c735995e3159c8fb
Esther Okiki <estherokiki15@gmail.com>
2025-03-05 20:21:05 +0100

    basic types

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
-let username = "Esther";
-console.log(username);
+// let username = "Esther";
+// console.log(username);
 
-let a: number = 12;
-let b: number = 6;
-let c: number = 2;
+// let a: number = 12;
+// let b: number = 6;
+// let c: number = 2;
 
-console.log(a / b);
+// console.log(a / b);
 
-console.log(c * b);
+// console.log(c * b);
+
+let myName: string = "Esther";
+let meaningOfLife: number;
+let isLoading: boolean;
+// let album: string | number;
+let album: any;
+
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = 1094;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean | string;
+
+let re: RegExp = /\w+/g;
+console.log(re);
